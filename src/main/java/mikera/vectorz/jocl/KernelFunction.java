b2e7c816db852936bc54d542e4bce27c4b9e1145
mikera <mike.r.anderson.13@gmail.com>
2016-09-08 12:27:05 +0800

    Implement ThreadLocal cache for kernel functions

--- src/main/java/mikera/vectorz/jocl/KernelFunction.java
+++ src/main/java/mikera/vectorz/jocl/KernelFunction.java
@@ -5,21 +5,47 @@ import static org.jocl.CL.clReleaseKernel;
 
 import org.jocl.cl_kernel;
 
+/**
+ * Encapsulates an OpenCL Kernel function
+ * 
+ * Manages a thread-local cache of cl_kernel objects for this kernel function.
+ * 
+ * @author Mike
+ *
+ */
 public class KernelFunction {
-	private final cl_kernel kernel;
-	
-	public KernelFunction(Program program, String name) {
-		this.kernel=clCreateKernel(program.program, name, null);
-	}
+	private final Program program;
+	private final String name;
 
-	@Override
-	public void finalize() throws Throwable {
-		clReleaseKernel(getKernel());
-		super.finalize();
+	public KernelFunction(Program program, String name) {
+		this.program=program;
+		this.name=name;
+	}
+	
+	private final ThreadLocal<KernelWrapper> cache=new ThreadLocal<>();
+	
+	private class KernelWrapper {
+		private final cl_kernel kernel;
+		
+		public KernelWrapper() {
+			this.kernel=clCreateKernel(program.program, name, null);
+		}
+
+		@Override
+		public void finalize() throws Throwable {
+			clReleaseKernel(getKernel());
+			super.finalize();
+		}
 	}
 
 	public cl_kernel getKernel() {
-		return kernel;
+		KernelWrapper k=cache.get();
+		if (k==null) {
+			k=new KernelWrapper();
+			cache.set(k);
+		}
+		
+		return k.kernel;
 	}
 
 }
